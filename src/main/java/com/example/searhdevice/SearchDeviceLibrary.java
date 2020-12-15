9357eec7f12f947a3393a243b13925ac3ccb54f1
DEV-GUSTAVO-PALACIOS <gustavo.palacios@DEV-GUSTAVO-PALACIOS.local>
2020-12-15 15:59:51 -0600

    Initial commit

---
+++ src/main/java/com/example/searhdevice/SearchDeviceLibrary.java
@@ -0,0 +1,11 @@
+package com.example.searhdevice;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class SearchDeviceLibrary {
+
+    public  void search(String saludo){
+        Log.i("prueba :: ","Ya estamos dentro de la libreria :: " + saludo);
+    }
+}
